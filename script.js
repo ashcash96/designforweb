4e670e56141eb9fb2500fdbd2e3be688bd7dbc41
ashcash96 <ashley.luciano@purchase.edu>
2018-05-14 15:39:33 -0400

    h
    j

---
+++ script.js
@@ -0,0 +1,92 @@
+$(function() {
+  $('#arena').hover(function() {
+    $('#one').css('display', 'block');
+  }, function() {
+    $('#one').css('display', 'none');
+  });
+});
+
+$(function() {
+  $('#html').hover(function() {
+    $('#two').css('display', 'block');
+  }, function() {
+    $('#two').css('display', 'none');
+  });
+});
+
+$(function() {
+  $('#mash').hover(function() {
+    $('#three').css('display', 'block');
+  }, function() {
+    $('#three').css('display', 'none');
+  });
+});
+
+$(function() {
+  $('#clock').hover(function() {
+    $('#four').css('display', 'block');
+  }, function() {
+    $('#four').css('display', 'none');
+  });
+});
+
+
+
+$(function() {
+  $('#life').hover(function() {
+    $('#five').css('display', 'block');
+  }, function() {
+    $('#five').css('display', 'none');
+  });
+});
+$(function() {
+  $('#ani').hover(function() {
+    $('#six').css('display', 'block');
+  }, function() {
+    $('#six').css('display', 'none');
+  });
+});
+$(function() {
+  $('#hover').hover(function() {
+    $('#seven').css('display', 'block');
+  }, function() {
+    $('#seven').css('display', 'none');
+  });
+});
+$(function() {
+  $('#flex').hover(function() {
+    $('#eight').css('display', 'block');
+  }, function() {
+    $('#eight').css('display', 'none');
+  });
+});
+
+
+$(function() {
+  $('#jq').hover(function() {
+    $('#nine').css('display', 'block');
+  }, function() {
+    $('#nine').css('display', 'none');
+  });
+});
+$(function() {
+  $('#pages').hover(function() {
+    $('#ten').css('display', 'block');
+  }, function() {
+    $('#ten').css('display', 'none');
+  });
+});
+$(function() {
+  $('#portal').hover(function() {
+    $('#eleven').css('display', 'block');
+  }, function() {
+    $('#eleven').css('display', 'none');
+  });
+});
+$(function() {
+  $('#select').hover(function() {
+    $('#twelve').css('display', 'block');
+  }, function() {
+    $('#twelve').css('display', 'none');
+  });
+});
